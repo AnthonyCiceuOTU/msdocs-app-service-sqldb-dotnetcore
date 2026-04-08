40b90349eb3c64a62bc8e1491cf8162f0415f245
aj20l <156533724+aj20l@users.noreply.github.com>
2026-04-08 05:36:23 -0400

    Unit 5 addition

---
+++ ViewModels/ListsViewModel.cs
@@ -0,0 +1,12 @@
+namespace DotNetCoreSqlDb.ViewModels
+{
+    public class ListsViewModel
+    {
+        public string UserAnswer { get; set; }
+        public bool ShowHint { get; set; }
+        public bool ShowSolution { get; set; }
+        public bool? IsCorrect { get; set; }
+        public string FeedbackMessage { get; set; }
+    }
+
+}
