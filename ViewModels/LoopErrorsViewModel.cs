42fb8887fa3b1d2ea0f6d81d9ce8a8b723184037
aj20l <156533724+aj20l@users.noreply.github.com>
2026-04-12 19:54:39 -0400

    unit 4  update

--- ViewModels/LoopErrorsViewModel.cs
+++ ViewModels/LoopErrorsViewModel.cs
@@ -1,6 +1,23 @@
 namespace DotNetCoreSqlDb.ViewModels
 {
-    public class LoopErrorsViewModel : UnitFourLessonViewModel
+    public class LoopErrorsViewModel
     {
+
+        public string UserAnswer { get; set; }
+
+        public bool ShowHint { get; set; }
+        public bool ShowSolution { get; set; }
+
+        public bool? IsCorrect { get; set; }
+        public string FeedbackMessage { get; set; }
+
+
+        public string ExplanationAnswer { get; set; }
+
+        public bool? ExplanationCorrect { get; set; }
+        public string ExplanationFeedback { get; set; }
+
+
+        public string ActionType { get; set; }
     }
 }
