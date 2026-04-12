0d576c3b49cc99ddc87a31b1f58d51f2a56424ff
aj20l <156533724+aj20l@users.noreply.github.com>
2026-04-12 18:32:29 -0400

    Update ListsViewModel.cs

--- ViewModels/ListsViewModel.cs
+++ ViewModels/ListsViewModel.cs
@@ -2,11 +2,18 @@ namespace DotNetCoreSqlDb.ViewModels
 {
     public class ListsViewModel
     {
+
         public string UserAnswer { get; set; }
-        public bool ShowHint { get; set; }
-        public bool ShowSolution { get; set; }
         public bool? IsCorrect { get; set; }
         public string FeedbackMessage { get; set; }
-    }
 
+
+        public bool ShowHint { get; set; }
+        public bool ShowSolution { get; set; }
+
+
+        public string ExplanationAnswer { get; set; }
+        public bool? ExplanationCorrect { get; set; }
+        public string ExplanationFeedback { get; set; }
+    }
 }
