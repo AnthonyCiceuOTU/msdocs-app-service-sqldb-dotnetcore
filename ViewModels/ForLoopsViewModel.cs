4e622701df414cfdfbc996d7ad6b3cc415315516
aj20l <156533724+aj20l@users.noreply.github.com>
2026-04-12 20:43:40 -0400

    Revert "unit 4  update"
    This reverts commit 42fb8887fa3b1d2ea0f6d81d9ce8a8b723184037.

--- ViewModels/ForLoopsViewModel.cs
+++ ViewModels/ForLoopsViewModel.cs
@@ -1,23 +1,6 @@
 namespace DotNetCoreSqlDb.ViewModels
 {
-    public class ForLoopsViewModel
+    public class ForLoopsViewModel : UnitFourLessonViewModel
     {
-
-        public string UserAnswer { get; set; }
-
-        public bool ShowHint { get; set; }
-        public bool ShowSolution { get; set; }
-
-        public bool? IsCorrect { get; set; }
-        public string FeedbackMessage { get; set; }
-
-
-        public string ExplanationAnswer { get; set; }
-
-        public bool? ExplanationCorrect { get; set; }
-        public string ExplanationFeedback { get; set; }
-
-
-        public string ActionType { get; set; }
     }
 }
