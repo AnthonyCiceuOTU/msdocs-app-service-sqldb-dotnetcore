e846f5f3e09670238f8bca70da50fee8152faa1a
aj20l <156533724+aj20l@users.noreply.github.com>
2026-04-14 19:49:30 -0400

    update viewmodels

--- ViewModels/SimulationViewModel.cs
+++ ViewModels/SimulationViewModel.cs
@@ -2,14 +2,23 @@ namespace DotNetCoreSqlDb.ViewModels
 {
     public class SimulationViewModel
     {
-        public string UserAnswer { get; set; }
+        public string Q1Answer { get; set; }
+        public string Q2Answer { get; set; }
+        public string Q3Answer { get; set; }
+
+        public bool? IsQ1Correct { get; set; }
+        public bool? IsQ2Correct { get; set; }
+        public bool? IsQ3Correct { get; set; }
+
+        public string Feedback1 { get; set; }
+        public string Feedback2 { get; set; }
+        public string Feedback3 { get; set; }
+
+        public string ExplanationAnswer { get; set; }
+        public bool? ExplanationCorrect { get; set; }
+        public string ExplanationFeedback { get; set; }
 
         public bool ShowHint { get; set; }
         public bool ShowSolution { get; set; }
-
-        public bool? IsCorrect { get; set; }
-        public string FeedbackMessage { get; set; }
-
-        public string ActionType { get; set; }
     }
 }
